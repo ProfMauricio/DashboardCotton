ca68cd99122a5e7106c6e04070bf9aaaa11d5bef
Mauricio F. L. Pereira <54080756+ProfMauricio@users.noreply.github.com>
2025-07-22 09:48:50 -0300

    Ajustes na leitura dos dados de gestao agricola

--- DashboardModelo1/dashboard_gestao_agricola.py
+++ DashboardModelo1/dashboard_gestao_agricola.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+from dados_fazenda import DadosFazenda
 
 def obter_dados_voos(lista_arq : list[str]) -> list :
     """
@@ -10,9 +11,9 @@ def obter_dados_voos(lista_arq : list[str]) -> list :
     :return:
     """
     global dados_agricolas_voos
-    for i, nome_arq in enumerate(lista_voos):
+    for i, nome_arq in enumerate(lista_arq):
         dados_voo = pd.read_csv(nome_arq)
-        id_voo = f'voo{i + 1}'
+        id_voo = f'Etapa {i + 1}'
         temp_dict = {id_voo: dados_voo}
         dados_agricolas_voos.append(dados_voo)
     return dados_agricolas_voos
@@ -36,25 +37,40 @@ def obter_dados_gli() -> pd.DataFrame:
 if __name__ == '__main__':
     # carregando os dados da base (planilhas)
     dados_agricolas_voos = []
-    lista_voos = ['./dadosPlanilha/voos/tabela_dados_voo1.csv',
-                  './dadosPlanilha/voos/tabela_dados_voo2.csv',
+    etapas = ['Etapa 1', 'Etapa 2', 'Etapa 3' ]
+    lista_voos = ['voo1', 'voo3', 'voo4']
+    lista_nome_arquivos = ['./dadosPlanilha/voos/tabela_dados_voo1.csv',
+                  #'./dadosPlanilha/voos/tabela_dados_voo2.csv',
                   './dadosPlanilha/voos/tabela_dados_voo3.csv',
                   './dadosPlanilha/voos/tabela_dados_voo4.csv']
-    obter_dados_voos(lista_voos)
+
+
+
+    # carregando os arquivos na memoria
+    leitura_dados = DadosFazenda()
+    # buscando os arquivos
+    for i,voo in enumerate(lista_voos):
+        resposta = leitura_dados.obter_voo(voo, lista_nome_arquivos[i])
+        if resposta :
+            print('Voo obtido com sucesso')
+
+    obter_dados_voos(lista_nome_arquivos)
+
     # ajustando o site para  gestão agrícola
     st.set_page_config(layout="wide")
     st.title("Gestão agrícola e recomendações para suporte a decisão")
     # filtro dos voos realizados
-    voo_selecionado = st.sidebar.selectbox("Dados de qual voo",
-                                           [f'voo{i+1}' for i in range(len(lista_voos))] )
+    voo_selecionado = st.sidebar.selectbox("Dados de qual etapa",
+                                           etapas )
 
-    lista_voo_indice = {f'voo{i+1}':i for i in range(len(lista_voos))}
+    lista_etapa_indice = {f'Etapa{i+1}':i for i in range(len(etapas))}
+
     # obtendo os dados do voo selecionado
-    dados_gestao_agro =  dados_agricolas_voos[lista_voo_indice[voo_selecionado]]
+    dados_gestao_agro =  dados_agricolas_voos[etapas.index(voo_selecionado)]
 
-    indice_filtro_ndvi={'voo1': 0.1,
-                        'voo2': 0.6,
-                        'voo3': 0.6,
+    indice_filtro_ndvi={'Etapa 1': 0.1,
+                        'Etapa 2': 0.6,
+                        'Etapa 3': 0.6,
                         'voo4': 0.6}
     dados_gestao_agro['ndvi_cores'] = dados_gestao_agro['ndvi_avg'].map(lambda x: 'red' if x < indice_filtro_ndvi[voo_selecionado] else 'green')
     # gráficos de NDVI
